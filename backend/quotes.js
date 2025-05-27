a58a8e7b0283e8d983e2e7919beb5095ac8c42d3
Rajiv Thapa <rajivthapa46@gmail.com>
2025-05-27 11:01:50 -0700

    add notes

--- backend/quotes.js
+++ backend/quotes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const server = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+server.get('/', (request, response) => {
+  response.send('Hello World');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
 server.use(express.json());
 
